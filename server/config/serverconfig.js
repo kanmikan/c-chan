7f4c6a448310f01f2944c4b635733e8c6d666cd0
kanmikan <soloquieronp@gmail.com>
2021-09-11 17:56:47 -0300

    variables de entorno

--- server/config/serverconfig.js
+++ server/config/serverconfig.js
@@ -1,17 +1,17 @@
 module.exports = {
 	DBNAME: "mikandbv2",
 	DBURL: process.env.MONGOURI || "mongodb://127.0.0.1:27017",
-	SSL: process.env.SSL || false,
+	SSL: ((process.env.SSL === "true") ? true : false) || false,
 	PORT: process.env.PORT || 3000,
 	SESSION_SECRET: process.env.SESSION_SECRET || "test",
-	DATABASE_CACHE: true,
-	STATIC_CACHE_VALUE: {},//{maxAge: 3600000*12},
+	DATABASE_CACHE: ((process.env.DATABASE_CACHE === "true") ? true : false) || true,
+	STATIC_CACHE_VALUE: process.env.STATIC_CACHE || {},//{maxAge: 3600000*12},
 	
 	/* APIS EXTERNAS */
 	YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
 	
 	/* CONFIGURACION DE SERVIDORES DE IMAGEN */
-	IMG_SERVER: 0, //0: local, 1: imgur, 2: imgbb, 3: cloudinary
+	IMG_SERVER: parseInt(process.env.IMG_SERVER) || 0, //0: local, 1: imgur, 2: imgbb, 3: cloudinary
 	IMG_LOCAL_THUMBNAIL_SIZE: 300, //300px
 	IMGUR_THUMBNAIL_QUALITY: "m", //l: large m: medium, etc.
 	CLOUDINARY_THUMBNAIL_CONFIG: "/c_scale,pg_1,w_200,f_auto/",
