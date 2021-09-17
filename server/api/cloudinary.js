ae51f59c0b0d000c3ea0618dec5218f33931e219
kanmikan <soloquieronp@gmail.com>
2021-09-17 16:15:20 -0300

    fix del thumbnail de cloudinary

--- server/api/cloudinary.js
+++ server/api/cloudinary.js
@@ -34,7 +34,8 @@ function genCloudyThumb(url, video=false){
 	let o = n[n.length-1].split(".");
 	o[1] = ".webp"; //asignar formato webp en la url.
 	let sliceA = url.slice(0, url.lastIndexOf("/"));
-	return url.slice(0, sliceA.lastIndexOf("/")) + (video) ? "/so_auto/" : "" + sConfig.CLOUDINARY_THUMBNAIL_CONFIG + n[n.length - 2] + "/" + o[0]+o[1];
+	let vcfg = (video) ? "/so_auto/" : "";
+	return url.slice(0, sliceA.lastIndexOf("/")) + vcfg + sConfig.CLOUDINARY_THUMBNAIL_CONFIG + n[n.length - 2] + "/" + o[0]+o[1];
 }
 
 module.exports = {uploadImg, uploadVid, genCloudyThumb}
