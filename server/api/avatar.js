94506d911887411114e1cb58bcff279a98f967e5
kanmikan <soloquieronp@gmail.com>
2021-10-22 15:57:48 -0300

    autoconvertir urls a https

--- server/api/avatar.js
+++ server/api/avatar.js
@@ -1,62 +1,64 @@
 /* MANEJA LOS ICONOS DE COMENTARIOS, SU FRECUENCIA, ETC */
+//TODO: anons festivos
 
-function genAnon(){
-	//se encarga de elegir que set de anons usar, dependiendo de variables como los dias festivos, etc.
-	return pickCSSAnon();
-	
+function genAnon() {
+    //se encarga de elegir que set de anons usar, dependiendo de variables como los dias festivos, etc.
+    return pickCSSAnon();
+
 }
 
-function pickCSSAnon(){
-	let anone = [
-		["ico,#FFcc00,#ffffff", 1], //amarillo
-		["ico,#0579b3,#ffffff", 1], //azul
-		["ico,#02b13c,#ffffff", 1], //verde
-		["ico,#df0202,#ffffff", 1], //rojo
-		["class,anonMulti,white", 0.1], //multi
-		["class,anonInvertido,white", 0.1], //invertido
-		["ico,#000000,#ffffff", 0.01], //black
-		["/assets/anon/8.png", 0.001], //uff
-		["ico,#ffffff,#000000", 0.01] //white
-	];
-	return weightRandom(anone);
+function pickCSSAnon() {
+    let anone = [
+        ["ico,#FFcc00,#ffffff", 1], //amarillo
+        ["ico,#0579b3,#ffffff", 1], //azul
+        ["ico,#02b13c,#ffffff", 1], //verde
+        ["ico,#df0202,#ffffff", 1], //rojo
+        ["class,anonMulti,white", 0.1], //multi
+        ["class,anonInvertido,white", 0.1], //invertido
+        ["ico,#000000,#ffffff", 0.01], //black
+        ["/assets/anon/8.png", 0.001], //uff
+        ["ico,#ffffff,#000000", 0.01] //white
+    ];
+    return weightRandom(anone);
 }
 
-function pickDefaultAnon(){
-	//elemento, porcentaje de chance
-	let anone = [
-		["/assets/anon/1.png", 1], //amarillo
-		["/assets/anon/2.png", 1], //azul
-		["/assets/anon/3.png", 1], //verde
-		["/assets/anon/4.png", 1], //rojo
-		["/assets/anon/5.gif", 0.1], //multi
-		["/assets/anon/6.gif", 0.1], //invertido
-		["/assets/anon/7.png", 0.01], //black
-		["/assets/anon/8.png", 0.001], //uff
-		["/assets/anon/9.png", 0.01] //white
-	];
-	return weightRandom(anone);
+//DEPRECATED
+function pickDefaultAnon() {
+    //elemento, porcentaje de chance
+    let anone = [
+        ["/assets/anon/1.png", 1], //amarillo
+        ["/assets/anon/2.png", 1], //azul
+        ["/assets/anon/3.png", 1], //verde
+        ["/assets/anon/4.png", 1], //rojo
+        ["/assets/anon/5.gif", 0.1], //multi
+        ["/assets/anon/6.gif", 0.1], //invertido
+        ["/assets/anon/7.png", 0.01], //black
+        ["/assets/anon/8.png", 0.001], //uff
+        ["/assets/anon/9.png", 0.01] //white
+    ];
+    return weightRandom(anone);
 }
 
-function weightRandom(data){
-	let elements = new Array();
-	let probability = new Array();
-	
-	for (var i=0; i<data.length; i++){
-		elements.push(data[i][0]);
-		probability.push(data[i][1]);
-	}
-	
-	let total = eval(probability.join("+"));
-	let weighted = new Array();
-	let current = 0;
-	while(current < probability.length){
-		for(i=0; i<probability[current]; i++){
-			weighted[weighted.length] = elements[current];
-		}
-		current++;
-	}
-	let random = Math.floor(Math.random() * total);
-	return weighted[random];
+function weightRandom(data) {
+    let elements = new Array();
+    let probability = new Array();
+
+    for (var i = 0; i < data.length; i++) {
+        elements.push(data[i][0]);
+        probability.push(data[i][1]);
+    }
+
+    let total = eval(probability.join("+"));
+    let weighted = new Array();
+    let current = 0;
+    while (current < probability.length) {
+        for (i = 0; i < probability[current]; i++) {
+            weighted[weighted.length] = elements[current];
+        }
+        current++;
+    }
+    let random = Math.floor(Math.random() * total);
+    return weighted[random];
 }
 
-module.exports = {genAnon}
+module.exports = { genAnon }
--- server/utils.js
+++ server/utils.js
@@ -1,49 +1,49 @@
-const {v4: uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 /* FUNCIONES UTILITARIAS DE USO EN EL SERVER Y EN EL RENDER */
-function clone(json){
-	return JSON.parse(JSON.stringify(json));
+function clone(json) {
+    return JSON.parse(JSON.stringify(json));
 }
 
-function getCategoryData(categorias, catid){
-	let default_ = {content: {media: {icon: "/assets/logo.png", image: "/assets/logo.png", misc: []}}}
-	if (categorias[0] === undefined){return default_;}
-	let filteredCat = categorias.filter( item => item.catid === catid)[0];
-	return (filteredCat) ? filteredCat : default_;
+function getCategoryData(categorias, catid) {
+    let default_ = { content: { media: { icon: "/assets/logo.png", image: "/assets/logo.png", misc: [] } } }
+    if (categorias[0] === undefined) { return default_; }
+    let filteredCat = categorias.filter(item => item.catid === catid)[0];
+    return (filteredCat) ? filteredCat : default_;
 }
 
-function filterComMedia(comments){
-	if (comments){
-		return comments.filter( item => item.img.full != "");
-	} else {
-		return comments;
-	}
+function filterComMedia(comments) {
+    if (comments) {
+        return comments.filter(item => item.img.full != "");
+    } else {
+        return comments;
+    }
 }
 
-function getCatShow(categoria){
-	//añadir parametros especiales a las categorias, ejemplo: oficial
-	if (categoria === "oficial"){return "<span>Oficial</span>";}
-	return categoria.toUpperCase();
+function getCatShow(categoria) {
+    //añadir parametros especiales a las categorias, ejemplo: oficial
+    if (categoria === "oficial") { return "<span>Oficial</span>"; }
+    return categoria.toUpperCase();
 }
 
 //FUNCION: genera un hash de color random basado en un seed.
 function genColor(str) {
-	var hash = 0;
-	for (var i = 0; i < str.length; i++) {
-		hash = str.charCodeAt(i) + ((hash << 5) - hash);
-	}
-	var colour = '#';
-	for (var i = 0; i < 3; i++) {
-		var value = (hash >> (i * 8)) & 0xFF;
-		colour += ('00' + value.toString(16)).substr(-2);
-	}
-	return colour;
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    var colour = '#';
+    for (var i = 0; i < 3; i++) {
+        var value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
 }
 
 //FUNCION: genera un string random basado en un seed.
 //https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
 function xmur3(str) {
-    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
+    for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
         h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
         h = h << 13 | h >>> 19;
     return function() {
@@ -54,79 +54,85 @@ function xmur3(str) {
 }
 
 function timeSince(timestamp) {
-	var now = new Date(); 
-	var secondsPast = (now.getTime() - timestamp) / 1000;
-	
-	if (secondsPast < 60) {
-		return parseInt(secondsPast) + 's';
-	} else if (secondsPast < 3600) {
-		return parseInt(secondsPast / 60) + 'm';
-	} else if (secondsPast < 86400) {
-		return parseInt(secondsPast / 3600) + 'h';
-	} else if (secondsPast < 2678400) {
-		return parseInt(secondsPast / 86400) + 'd';
-	} else if (secondsPast <= 32140800) {
-		return parseInt(secondsPast / 2678400) + 'ms';
-	} else if (secondsPast > 32140800) {
-		return parseInt(secondsPast / 32140800) + 'a';
-	}	
+    var now = new Date();
+    var secondsPast = (now.getTime() - timestamp) / 1000;
+
+    if (secondsPast < 60) {
+        return parseInt(secondsPast) + 's';
+    } else if (secondsPast < 3600) {
+        return parseInt(secondsPast / 60) + 'm';
+    } else if (secondsPast < 86400) {
+        return parseInt(secondsPast / 3600) + 'h';
+    } else if (secondsPast < 2678400) {
+        return parseInt(secondsPast / 86400) + 'd';
+    } else if (secondsPast <= 32140800) {
+        return parseInt(secondsPast / 2678400) + 'ms';
+    } else if (secondsPast > 32140800) {
+        return parseInt(secondsPast / 32140800) + 'a';
+    }
 }
 
-function formatBytes(a,b=2){
-	if (0===a) return "0 Bytes";
-	const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));
-	return parseFloat((a/Math.pow(1024,d)).toFixed(c))+" "+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d];
+function formatBytes(a, b = 2) {
+    if (0 === a) return "0 Bytes";
+    const c = 0 > b ? 0 : b,
+        d = Math.floor(Math.log(a) / Math.log(1024));
+    return parseFloat((a / Math.pow(1024, d)).toFixed(c)) + " " + ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][d];
 }
 
-function genCID(len){
-	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	var result = "";
-	for (var i = len; i > 0; --i){
-		result += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return result;
+function genCID(len) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var result = "";
+    for (var i = len; i > 0; --i) {
+        result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
 }
 
-function randomString(len){
-	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	var result = "";
-	for (var i = len; i > 0; --i){
-		result += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return result;
+function randomString(len) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    var result = "";
+    for (var i = len; i > 0; --i) {
+        result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
 }
 
-function isGif(url){
-	return url.slice(-4) === ".gif";
+function isGif(url) {
+    return url.slice(-4) === ".gif";
 }
 
-function isImg(url){
-	let match = url.match(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i)
-	return (match) ? true : false;
+function isImg(url) {
+    let match = url.match(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i)
+    return (match) ? true : false;
 }
 
-function isVideo(url){
-	let match = url.match(/\.(webm|mp4)$/i)
-	return (match) ? true : false;
+function isVideo(url) {
+    let match = url.match(/\.(webm|mp4)$/i)
+    return (match) ? true : false;
 }
 
-function getPollPercent(poll1, poll2){
-	var total = poll1+poll2;
-	if (total === 0) return ["",""];
-	//sacar porcentaje de diferencia segun cantidad de votos
-	var per = (poll1/total) * 100;
-	var per2 = (poll2/total) * 100;
-	return [Math.round(per) + "%", Math.round(per2) + "%"];
+function getPollPercent(poll1, poll2) {
+    var total = poll1 + poll2;
+    if (total === 0) return ["", ""];
+    //sacar porcentaje de diferencia segun cantidad de votos
+    var per = (poll1 / total) * 100;
+    var per2 = (poll2 / total) * 100;
+    return [Math.round(per) + "%", Math.round(per2) + "%"];
 }
 
-function parseCookies(raw){
-	let cookie = raw.split(";");
-	let obj = {};
-	for (var i=0; i<cookie.length; i++){
-		let data = cookie[i].split("=");
-		obj[data[0].trim()] = data[1];
-	}
-	return obj;
+function parseCookies(raw) {
+    let cookie = raw.split(";");
+    let obj = {};
+    for (var i = 0; i < cookie.length; i++) {
+        let data = cookie[i].split("=");
+        obj[data[0].trim()] = data[1];
+    }
+    return obj;
 }
 
-module.exports = {isGif, isImg, isVideo, clone, getCategoryData, filterComMedia, getCatShow, timeSince, formatBytes, getPollPercent, genCID, uuidv4, randomString, parseCookies, genColor, xmur3};
+//FUNCION: convertir a https
+function seclink(url) {
+    return url.replace("http://", "https://");
+}
+
+module.exports = { seclink, isGif, isImg, isVideo, clone, getCategoryData, filterComMedia, getCatShow, timeSince, formatBytes, getPollPercent, genCID, uuidv4, randomString, parseCookies, genColor, xmur3 };
