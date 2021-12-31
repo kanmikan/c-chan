3126e60631df1ee547d96a559280118150d89ea5
kanmikan <soloquieronp@gmail.com>
2021-12-31 19:26:12 -0300

    fix del malbot

---
+++ server/extra/contentinterface.js
@@ -0,0 +1,37 @@
+const dbManager = require('../db/dbmanager.js');
+const mdbScheme = require('../db/models/mdbscheme.js');
+const jsonScheme = require('../db/models/jsonscheme.js');
+const avatar = require('../api/avatar.js');
+const utils = require('../utils.js');
+const pass = require('../api/passport.js');
+const live = require('../api/live.js');
+const cparser = require('../api/parser.js');
+
+async function createCom(DB, data, before, callback){
+	before();
+	let mcom = utils.clone(jsonScheme.COMMENT_SCHEME);
+	mcom.cid = utils.genCID(7);
+	mcom.bid = data.bid;
+	mcom.user.uid = data.user.uid;
+	mcom.user.jerarquia = data.user.jerarquia;
+	
+	mcom.type = data.type;
+	mcom.img = data.img;
+	
+	mcom.date.created = Date.now();
+	mcom.icon = avatar.genAnon(data.type);
+	mcom.content.body = data.comment;
+	
+	//insertar comentario
+	await dbManager.insertDB(DB, mdbScheme.C_COMS, mcom, () => {});
+	callback({success: true, data: mcom});
+	
+	//enviar comentario via socket
+	let protectedJSON = pass.filterProtectedUID(mcom);
+	live.sendDataTo(data.bid, "comment", {token: "no-token", op: false, data: protectedJSON});
+	//enviar señal de nueva actividad a todos
+	live.sendData("activity", {kind: "comment", data: protectedJSON});
+	
+}
+
+module.exports = {createCom}
--- server/extra/malbot.js
+++ server/extra/malbot.js
@@ -2,11 +2,13 @@
 const dbManager = require('../db/dbmanager.js');
 const mdbScheme = require('../db/models/mdbscheme.js');
 const jsonScheme = require('../db/models/jsonscheme.js');
-const cparser = require('../api/parser.js');
 const utils = require('../utils.js');
 const live = require('../api/live.js');
 const pass = require('../api/passport.js');
 const Parser = require('rss-parser');
+const malScraper = require('mal-scraper');
+const cinterface = require('./contentinterface.js');
+const translate = require('@vitalets/google-translate-api');
 
 let parser = new Parser({
 	customFields: {
@@ -14,6 +16,162 @@ let parser = new Parser({
 		item: ['media:thumbnail'],
 	}
 });
+
+function similarAnimes(DB, bid, criteria, callback){
+
+	
+}
+
+function listAnimes(DB, bid, criteria, callback){
+	console.log("[MalBot] Esta buscando informacion en MyAnimeList...");
+	
+	let params = criteria.split(" ");
+	
+	malScraper.getSeason(params[0], params[1])
+		.then((data) => {
+			
+			let animes = [];
+			for (var i=0; i<data.TV.length; i++){
+				animes.push({name: data.TV[i].title, url: data.TV[i].link});
+			}
+			
+			let cdata = {
+				bid: bid,
+				user: {
+					uid: "malbot",
+					jerarquia: {
+						nick: "MalBot",
+						rango: "Bot",
+						color: "#3f51b5"
+					}
+				},
+				comment: "",
+				type: [],
+				img: {
+					preview: "",
+					full: "",
+					raw: ""
+				}
+			};
+			
+			cdata.comment = `<div class="comObject" style="border-radius: 5px;padding: 10px;"><span style="color: orange">Resultados de "${criteria}":</span></br><ul>`;
+			
+			animes.forEach(function(anm){
+				cdata.comment += `<li><a href="${anm.url}">${anm.name}</a></li>`;
+			});
+			cdata.comment += `</ul></div>`;
+			
+			//crear comentario.
+			cinterface.createCom(DB, cdata, function(){
+				console.log("[MalBot] Creando comentario con la informacion obtenida.");
+			}, function(result){
+				callback(result);
+			});
+			
+		}).catch((err) => {
+			console.log(err);
+		});
+	
+}
+
+function previewAnime(DB, bid, name, callback){
+	console.log("[MalBot] Esta buscando informacion en MyAnimeList...");
+	malScraper.getInfoFromName(name)
+		.then((data) => {
+			
+			let cdata = {
+				bid: bid,
+				user: {
+					uid: "malbot",
+					jerarquia: {
+						nick: "MalBot",
+						rango: "Bot",
+						color: "#3f51b5"
+					}
+				},
+				comment: "",
+				type: ["image"],
+				img: {
+					preview: data.picture,
+					full: data.picture,
+					raw: ""
+				}
+			};
+			
+			translateCallback(data.synopsis, function(tres){
+				
+				cdata.comment = `<div class="comObject" style="border-radius: 5px;padding: 10px;">
+				<span>${data.title} - ${data.premiered}</span>
+				</br>
+				<a href="${data.url}">${data.url}</a>
+				</br>
+				<span>${tres.text}</span>
+				</br>
+				</div>`;
+				
+				//crear comentario.
+				cinterface.createCom(DB, cdata, function(){
+					console.log("[MalBot] Creando comentario con la informacion obtenida.");
+				}, function(result){
+					callback(result);
+				});
+				
+			});
+			
+		}).catch((err) => {
+			console.log(err);
+		});
+}
+
+function searchAnime(DB, bid, query, callback){
+	console.log("[MalBot] Esta buscando informacion en MyAnimeList...");
+	malScraper.getResultsFromSearch(query)
+		.then((data) => {
+			//construir cuerpo del comentario y enviar los datos al commentInterface
+			//console.log(data);
+			
+			let animes = [];
+			for (var i=0; i<data.length; i++){
+				animes.push({name: data[i].name, url: data[i].url});
+			}
+			
+			let cdata = {
+				bid: bid,
+				user: {
+					uid: "malbot",
+					jerarquia: {
+						nick: "MalBot",
+						rango: "Bot",
+						color: "#3f51b5"
+					}
+				},
+				comment: "",
+				type: [],
+				img: {
+					preview: "",
+					full: "",
+					raw: ""
+				}
+			};
+			
+			cdata.comment = `<div class="comObject" style="border-radius: 5px;padding: 10px;"><span style="color: orange">Resultados de "${query}":</span></br><ul>`;
+			
+			animes.forEach(function(anm){
+				cdata.comment += `<li><a href="${anm.url}">${anm.name}</a></li>`;
+			});
+			cdata.comment += `</ul></div>`;
+			
+			//crear comentario.
+			cinterface.createCom(DB, cdata, function(){
+				console.log("[MalBot] Creando comentario con la informacion obtenida.");
+			}, function(result){
+				callback(result);
+			});
+		}).catch((err) => {
+			console.log(err);
+		})
+	
+}
 
 async function check(DB, id, callback){
 	let feed = await parser.parseURL('https://myanimelist.net/rss/news.xml');
@@ -47,6 +205,8 @@ async function check(DB, id, callback){
 }
 
 function createBox(DB, data, callback){
+	let cparser = require('../api/parser.js');
+
 	let time = Date.now();
 	
 	//let bid = utils.uuidv4();
@@ -88,4 +248,13 @@ function createBox(DB, data, callback){
 	});
 }
 
-module.exports = {check}
+function translateCallback(text, callback){
+	translate(text, {from: "en", to: "es"})
+		.then((res) => {
+			callback(res);
+		}).catch((err) => {
+			callback(null);
+		});
+}
+
+module.exports = {check, searchAnime, previewAnime, listAnimes, similarAnimes}
